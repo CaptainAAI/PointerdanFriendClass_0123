366b8f7f64736fcd683e78bc28a43f6c2e0e984b
CaptainAAI <anandaagungismail@gmail.com>
2024-05-06 10:36:45 +0700

    pointer reference refMhs

--- PointerdanFriendClass_0123/PointerdanFriendClass_0123.cpp
+++ PointerdanFriendClass_0123/PointerdanFriendClass_0123.cpp
@@ -16,6 +16,10 @@ int main()
 	mahasiswa mhs{ 1 }; //Obejct mhs
 	mhs.showNim();	//Member acces operator
 
+	mahasiswa& refMhs = mhs;	//Pointer reference refMhs
+	refMhs.nim = 2;		//member Acces Operator
+	mhs.showNim();
+
 	
 
 }
