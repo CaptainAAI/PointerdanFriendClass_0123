0d1fea90eba49f039a1fd3b4ed5bc353f62474d1
CaptainAAI <anandaagungismail@gmail.com>
2024-05-06 10:47:32 +0700

    class orang and its variable declaration

--- friendClassSatu/friendClassSatu.cpp
+++ friendClassSatu/friendClassSatu.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 using namespace std;
+#include<string>
+
+class siswa;
+class orang
+{
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+
